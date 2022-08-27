9701d6e2e42758b6d6e6035a0ad3d33accb3cade
JohnLee <lijiang01001@163.com>
2022-08-27 17:37:02 +0800

    bloom

--- Assets/JRP/Runtime/PostFXSettings.cs
+++ Assets/JRP/Runtime/PostFXSettings.cs
@@ -5,7 +5,33 @@ public class PostFXSettings : ScriptableObject
 {
     [SerializeField]
     Shader shader = default;
-    
+
+    [System.Serializable]
+    public struct BloomSettings {
+
+        [Range(0f, 16f)]
+        public int maxIterations;
+
+        [Min(1f)]
+        public int downscaleLimit;
+
+        public bool bicubicUpsampling;
+
+        [Min(0f)]
+        public float threshold;
+
+        [Range(0f, 1f)]
+        public float thresholdKnee;
+
+        [Min(0f)]
+        public float intensity;
+    }
+
+    [SerializeField]
+    BloomSettings bloom = default;
+
+    public BloomSettings Bloom => bloom;
+
     [System.NonSerialized]
     Material material;
 
