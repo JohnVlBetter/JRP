e8d85035758e1229ba2d4f199fae6fa29eaef6c3
lijiang <lijiang01001@163.com>
2024-09-29 18:49:15 +0800

    core init

---
+++ Assets/JRP/Runtime/Core/JObject.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+public class JObject : MonoBehaviour
+{
+    private JMaterial material;
+    private JMesh mesh;
+
+#if UNITY_EDITOR
+    private MeshRenderer meshRenderer;
+    private MeshFilter meshFilter;
+#endif
+
+
+}
